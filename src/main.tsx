d8c14e610e52eac951ea30a625dc8a4473dd3b7b
yuta-toshino1931 <yuta-toshino@hiroba1931.co.jp>
2025-07-09 22:25:37 +0900

    不要なインポートを削除

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Vite React App のエントリーポイント
 // id="root" のDOMに対してReactをマウントする
