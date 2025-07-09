e53762d723b416e575515dfb71becf48bff962f7
yuta-toshino1931 <yuta-toshino@hiroba1931.co.jp>
2025-07-09 22:25:16 +0900

    サンプルアプリを作成

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// Vite React App のエントリーポイント
+// id="root" のDOMに対してReactをマウントする
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
